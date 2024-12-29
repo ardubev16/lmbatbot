2d20420038bf22a08b323a8e009ff3bff8f6e8bd
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-12-29 15:44:32 +0100

    feat: handle private mentions and tags

--- src/lmbatbot/tags.py
+++ src/lmbatbot/tags.py
@@ -45,7 +45,7 @@ def _parse_tagadd_command(effective_message: Message) -> TagAddArgs:
         msg = "No mentions found"
         raise CommandParsingError(msg)
 
-    deuped_mentions = {mention.lower() for mention in mentions}
+    deuped_mentions = set(map(str.lower, mentions))
     return TagAddArgs(group=next(iter(hashtags)).lower(), tags=list(deuped_mentions))
 
 
@@ -68,6 +68,22 @@ def _upsert_tag_group(chat_id: int, tag_group: TagAddArgs) -> UpsertResult:
         session.execute(insert_stmt)
 
     return UpsertResult.UPDATED if row_exists else UpsertResult.INSERTED
+
+
+async def _send_private_mentions(message: Message, mentioned_usernames: set[str]) -> None:
+    assert message.from_user
+
+    text = f"You got mentioned in <b>{message.chat.effective_name}</b> by {message.from_user.name}."
+    if username := message.from_user.username:
+        with contextlib.suppress(KeyError):
+            mentioned_usernames.remove(f"@{username.lower()}")
+
+    # TODO: temporary implementation, create a table ad-hoc
+    # https://github.com/ardubev16/lmbatbot/issues/12
+    for username, user_id in settings.GLOBAL_PVT_NOTIFICATION_USERS:
+        if username.lower() in mentioned_usernames:
+            logger.info("Sending private message to `%s`", user_id)
+            await message.reply_html(text, do_quote=message.build_reply_arguments(target_chat_id=user_id))
 
 
 async def taglist_cmd(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
@@ -127,7 +143,7 @@ async def tagdel_cmd(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 
     chat_id = update.effective_chat.id
 
-    hashtags = update.effective_message.parse_entities([MessageEntity.HASHTAG]).values()
+    hashtags = list(map(str.lower, update.effective_message.parse_entities([MessageEntity.HASHTAG]).values()))
     if len(hashtags) == 0:
         text = """\
 Invalid format. Please use the following format:
@@ -148,12 +164,21 @@ Invalid format. Please use the following format:
     await update.effective_chat.send_message(message)
 
 
+async def handle_message_mentions(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    assert update.effective_message
+    assert update.effective_message.from_user
+
+    mentions = set(map(str.lower, update.effective_message.parse_entities([MessageEntity.MENTION]).values()))
+    await _send_private_mentions(update.effective_message, mentions)
+
+
 async def handle_message_with_tags(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     assert update.effective_chat
     assert update.effective_message
     assert update.effective_user
 
-    tags = list(update.effective_message.parse_entities([MessageEntity.HASHTAG]).values())
+    tags = list(map(str.lower, update.effective_message.parse_entities([MessageEntity.HASHTAG]).values()))
+    mentions = list(map(str.lower, update.effective_message.parse_entities([MessageEntity.MENTION]).values()))
 
     with Session() as session:
         found_groups = session.scalars(
@@ -171,27 +196,17 @@ async def handle_message_with_tags(update: Update, _: ContextTypes.DEFAULT_TYPE)
         with contextlib.suppress(KeyError):
             tag_set.remove(f"@{username.lower()}")
 
-    await update.effective_message.reply_html(" ".join(tag_set))
+    if tag_set:
+        await update.effective_message.reply_html(" ".join(tag_set))
 
-    # TODO: temporary implementation, create a table ad-hoc
-    # https://github.com/ardubev16/lmbatbot/issues/12
-    message = f"You got mentioned in <b>{update.effective_chat.effective_name}</b> by {update.effective_user.name}."
-    for username, user_id in settings.GLOBAL_PVT_NOTIFICATION_USERS:
-        if username.lower() in tag_set:
-            logger.info("Sending private message to `%s`", user_id)
-            await update.effective_message.reply_html(
-                message,
-                do_quote=update.effective_message.build_reply_arguments(target_chat_id=user_id),
-            )
+    await _send_private_mentions(update.effective_message, tag_set.union(mentions))
 
 
 def handlers() -> list[TypedBaseHandler]:
-    taglist_handler = CommandHandler("taglist", taglist_cmd)
-    tagadd_handler = CommandHandler("tagadd", tagadd_cmd)
-    tagdel_handler = CommandHandler("tagdel", tagdel_cmd)
-    tag_handler = MessageHandler(
-        filters.Entity(constants.MessageEntityType.HASHTAG),
-        handle_message_with_tags,
-    )
-
-    return [taglist_handler, tagadd_handler, tagdel_handler, tag_handler]
+    return [
+        CommandHandler("taglist", taglist_cmd),
+        CommandHandler("tagadd", tagadd_cmd),
+        CommandHandler("tagdel", tagdel_cmd),
+        MessageHandler(filters.Entity(constants.MessageEntityType.HASHTAG), handle_message_with_tags),
+        MessageHandler(filters.Entity(constants.MessageEntityType.MENTION), handle_message_mentions),
+    ]
